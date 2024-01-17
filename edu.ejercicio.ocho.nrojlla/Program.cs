84185eefa93b08d930e56a10696d9b830833d26a
VvNicol <150023181+VvNicol@users.noreply.github.com>
2024-01-17 16:34:28 +0100

    Add files via upload

---
+++ edu.ejercicio.ocho.nrojlla/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace edu.ejercicio.ocho.nrojlla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] texto = { "Hola", "Adiós", "Nuevo", "Viejo", "Vamos" };
+
+            int lugar = Array.IndexOf(texto, "Adiós"); 
+            if(lugar == -1)
+            {
+                Console.WriteLine("No se encuentra");
+
+            }   else
+            {
+                Console.WriteLine("Posicion: " +  lugar);
+            }
+
+            //pedir palabra
+            Console.WriteLine("Ingrese una palabra para buscar");
+            string buscar = Console.ReadLine();
+     
+
+            if (texto.Contains(buscar))
+            {
+                Console.WriteLine("La palabra existe.");
+
+            }
+            else
+            {
+                Console.WriteLine("La palabra no existe, intentelo de nuevo");
+            }
+
+            //Array de enteros
+
+            Console.WriteLine("Numeros mayores que 3:");
+
+            int[] numeros = { 1, 5, 7, 8, 4, 3, 9 };
+
+            var mayores = numeros.Where(num => num > 3);
+
+            foreach (int numero in mayores)
+            {
+                Console.WriteLine(numero);
+            }
+
+        }
+    }
+}
